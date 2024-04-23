640f0ddba53e640de9b3042fe670771afc4fe0a8
bruzdaaneta <aneta.bruzda12@interia.pl>
2024-04-23 18:03:54 +0200

    prefix

--- script.js
+++ script.js
@@ -5,27 +5,68 @@ const countryCodeInput = document.getElementById('countryCode');
 const myForm = document.getElementById('form');
 const modal = document.getElementById('form-feedback-modal');
 const clicksInfo = document.getElementById('click-count');
+const vatUECheckbox = document.getElementById('vatUE');
+const vatNumberInput = document.getElementById('vatNumber');
+
+const countriesPrefix = {
+    "Austria": "AT",
+    "Belgium": "BE",
+    "Bulgaria": "BG",
+    "Croatia": "HR",
+    "Cyprus": "CY",
+    "Czechia:": "CZ",
+    "Denmark": "DK",
+    "Estonia": "EE",
+    "Finland": "FI",
+    "France": "FR",
+    "Greece": "EL",
+    "Spain": "NL",
+    "Ireland": "IE",
+    "Lithuania": "LT",
+    "Luxembourg": "LU",
+    "Latvia": "LV",
+    "Malta": "MT",
+    "Germany": "DE",
+    "Poland": "PL",
+    "Portugal": "PT",
+    "Romania": "RO",
+    "Slovakia": "SK",
+    "Slovenia": "SI",
+    "Sweden": "SE",
+    "Hungary": "HU",
+    "Italy": "IT" 
+};
+
+function fillUENumber() {
+    if (vatUECheckbox.checked) {
+        let prefix = createPrefix();
+        vatNumberInput.value = prefix + vatNumberInput.value;
+    }
+    else {
+        vatNumberInput.value = vatNumberInput.value.substring(2);
+    }
+}
+
+function disableUEVat() {
+    if (!(countryInput.value in countriesPrefix)) {
+        vatUECheckbox.checked = false;
+    }
+    else vatUECheckbox.checked = true;
+    fillUENumber();
+}
+
 
 function handleClick() {
     clickCount++;
     clicksInfo.innerText = clickCount;
 }
 
-
-async function fetchAnd() {
-    try {
-        const response = await fetch('https://restcountries.com/v3.1/all');
-        if (!response.ok) {
-            throw new Error('Błąd pobierania danych');
-        }
-        const data = await response.json();
-        const countries = data.map(country => country.name.common);
-        countryInput.innerHTML = countries.map(country => `<option value="${country}">${country}</option>`).join('');
-        getCountryByIP();
-    } catch (error) {
-        console.error('Wystąpił błąd:', error);
-    }
+function createPrefix() {
+    const country = countryInput.value;
+    const prefix = country.slice(0, 2);
+    return prefix.toUpperCase();
 }
+
 
 async function fetchAndFillCountries() {
     try {
@@ -42,6 +83,7 @@ async function fetchAndFillCountries() {
             countriesList.appendChild(option);
         });
         getCountryByIP();
+
     } catch (error) {
         console.error('Wystąpił błąd:', error);
     }
@@ -53,14 +95,9 @@ function getCountryByIP() {
         .then(response => response.json())
         .then(data => {
             const country = data.country;
-            //const option = document.createElement('option');
-            //option.value = country;
-            //option.textContent = country;
-            //option.selected = true; 
-            //countryInput.appendChild(option);
-            //document.getElementById('countryInput').value = country;
             countryInput.value = country;
             getCountryCode();
+            disableUEVat();
         })
         .catch(error => {
             console.error('Błąd pobierania danych z serwera GeoJS:', error);
@@ -91,12 +128,14 @@ function getCountryCode() {
     });
 }
 
+vatUECheckbox.addEventListener('change', fillUENumber);
 
 (() => {
     // nasłuchiwania na zdarzenie kliknięcia myszką
     document.addEventListener('click', handleClick);
     fetchAndFillCountries();
     countryInput.addEventListener('change', getCountryCode);
+    countryInput.addEventListener('change', disableUEVat);
    
 })()
 
